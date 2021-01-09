c3da305babcd5d3155c007fb7b46f6ed7dbf58e7
amangoyalji <amana6398@gmail.com>
2021-01-09 14:49:53 +0530

    Project Structure

---
+++ OnlineShoppingMartLti/src/main/java/com/lti/OnlineShoppingMartLti/OnlineShoppingMartLtiApplication.java
@@ -0,0 +1,13 @@
+package com.lti.OnlineShoppingMartLti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineShoppingMartLtiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineShoppingMartLtiApplication.class, args);
+	}
+
+}
---
+++ OnlineShoppingMartLti/src/test/java/com/lti/OnlineShoppingMartLti/OnlineShoppingMartLtiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lti.OnlineShoppingMartLti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineShoppingMartLtiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
